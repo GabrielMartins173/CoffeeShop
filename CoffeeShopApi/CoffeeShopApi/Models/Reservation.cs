e590f12d4eff3fc36ace5f5322688e4da8425c0f
Gabriel Martins <gabriel_martins1@dellteam.com>
2020-01-10 09:54:04 -0300

    Adding the connection string for the database

--- CoffeeShopApi/CoffeeShopApi/Models/Reservation.cs
+++ CoffeeShopApi/CoffeeShopApi/Models/Reservation.cs
@@ -15,11 +15,11 @@ namespace CoffeeShopApi.Models
         public string Name { get; set; }
 
         [Required]
-        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email in not valid")]
         public string Email { get; set; }
 
         [Required]
-        [RegularExpression("^[0-9]*$")]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Invalid Phone Number")]
         public string Phone { get; set; }
 
         [Required]
